d94c6e38a28a72f6e76a7d8bf0b8db8e3271985b
Jakob Madsen <jbsm15@student.aau.dk>
2018-05-23 12:26:18 +0000

    Added ghetto prepare file for local testing environments

---
+++ kubernetes/kubernetes_prepare.sh
@@ -0,0 +1,28 @@
+#/usr/bin/env bash
+
+# Script to avoid having to make users manually
+# NOTE: Only use this in testing environments where you need mass deployment
+
+test=$1
+
+# Create user with home folder
+useradd kubernetes
+# Check if string is empty for setting password immediately
+if [ "$test" != "" ]; then
+# If the string length is greater than or equals 8 then pipe to passwd
+	if [ ${#test} -ge 8 ]; then
+	    printf "$test\n$test" | passwd kubernetes
+	else
+		printf "Invalid password length\n"
+	    passwd kubernetes
+	fi
+else
+	passwd kubernetes
+fi
+
+# Give kubernetes user docker access (wheel)
+usermod -aG wheel kubernetes
+
+curl -L "https://gitlab.giraf.cs.aau.dk/tools/deployment/raw/master/kubernetes/kubernetes_deploy.sh" -s -o /home/kubernetes/kubernetes_deploy.sh
+chmod +x /home/kubernetes/kubernetes_deploy.sh
+cd /home/kubernetes
